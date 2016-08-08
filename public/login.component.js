92ced618ef4855628909851f3448b96e3dc32a97
William T <william.tam18@gmail.com>
2016-08-08 17:50:06 +0800

    register work

--- public/login.component.js
+++ public/login.component.js
@@ -2,6 +2,21 @@ angular.module('thriftyApp')
   .component('login', {
     templateUrl: 'login.template.html',
     controller: function ($http, $scope) {
-      
+      $scope.loggerIn = function() {
+        var data = {
+          email: $scope.loguser.email,
+          password: $scope.loguser.password
+        }
+
+        $http({
+          method: 'POST',
+          url: 'https:thrifty-api.herokuapp.com/login',
+          data: data
+        })
+        .success( function (data) {
+          console.log(data)
+          $location.path("/main")
+        })
+      }
     }
   })
