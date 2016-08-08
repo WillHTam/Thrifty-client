4debc04ab9e81decf4edf1c5b30b8e491348a0ae
Sprazzeus <sprazzeus@gmail.com>
2016-08-08 18:04:24 +0800

    updated new goal template html and component js

--- public/newgoal.component.js
+++ public/newgoal.component.js
@@ -1,5 +1,21 @@
 angular.module('thriftyApp')
   .component('newgoal', {
     templateUrl: 'newgoal.template.html',
-    controller: function () {}
-  })
+    controller: function ($http, $scope) {
+      $scope.sendData = function() {
+        var data = {
+          name: $scope.newgoal.name,
+          cost: $scope.newgoal.cost
+        }
+
+        $http({
+          method: 'POST',
+          url: 'https:thrifty-api.herokuapp.com/newgoal',
+          data: data
+        })
+        .success( function (data) {
+          console.log(data)
+          $location.path("/goalplan")
+        })
+      }
+  }})
