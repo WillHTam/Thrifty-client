74f9bbb1fad94a6ce13cce5bd501460602e71d55
William T <william.tam18@gmail.com>
2016-08-08 17:13:57 +0800

    add files for each of planned routes

--- public/core.js
+++ public/core.js
@@ -8,4 +8,31 @@ app.config(function ($routeProvider) {
     .when('/register', {
       template: '<register></register>'
     })
+    .when('/getstarted', {
+      template: '<start></start>'
+    })
+    .when('/login', {
+      template: '<login></login>'
+    })
+    .when('/home', {
+      template: '<dashboard></dashboard>'
+    })
+    .when('/newgoal', {
+      template: '<newgoal></newgoal>'
+    })
+    .when('/goalplan', {
+      template: '<goalplan></goalplan>'
+    })
+    .when('/editgoalplan', {
+      template: '<editplan></editplan>'
+    })
+    .when('/editgoal', {
+      template: '<editgoal></editgoalplan>'
+    })
+    .when('/deletegoal', {
+      template: '<deletegoal></deletegoal>'
+    })
+    .when('/account', {
+      template: '<account></account>'
+    })
 })
