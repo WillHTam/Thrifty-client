27971d53fea2115ad0cfafa9bfca95f33ab03136
William T <william.tam18@gmail.com>
2016-08-10 10:16:02 +0800

    changing dashboard html and js

--- public/dashboard.component.js
+++ public/dashboard.component.js
@@ -1,5 +1,10 @@
 angular.module('thriftyApp')
   .component('dashboard', {
     templateUrl: 'dashboard.template.html',
-    controller: function () {}
+    controller: function ($http, $scope) {
+      $http.get("https://thrifty-app.herokuapp.com/me")
+        .then(function (response) {
+          $scope.first_name = response.first_name
+        })
+    }
   })
