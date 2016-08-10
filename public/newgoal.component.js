74ad259d782426837270cc1d38d2473816a904c1
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 12:07:12 +0800

    adding carousel

--- public/newgoal.component.js
+++ public/newgoal.component.js
@@ -2,6 +2,15 @@ angular.module('thriftyApp')
   .component('newgoal', {
     templateUrl: 'newgoal.template.html',
     controller: function ($http, $scope, $location) {
+
+      $scope.icons = ["graduation-cap", "home", "plane", "car",]
+      // $scope.vm.arrayData = [
+      //   { src: 'image1.png' },
+      //   { src: 'image2.png' },
+      //   { src: 'image3.png' },
+      //   { src: 'image4.png' }
+      // ];
+
       $scope.sendData = function() {
         var data = {
           name: $scope.goal.name,
