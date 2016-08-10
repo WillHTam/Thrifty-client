dec957a0b82147460cf4c2c2bfdc1a2ccda17dd6
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 12:50:42 +0800

    added looping icons. needs to POST goal.icon in sendData()

--- public/newgoal.component.js
+++ public/newgoal.component.js
@@ -1,36 +1,46 @@
 angular.module('thriftyApp')
-  .component('newgoal', {
-    templateUrl: 'newgoal.template.html',
-    controller: function ($http, $scope, $location) {
+.component('newgoal', {
+  templateUrl: 'newgoal.template.html',
+  controller: function ($http, $scope, $location) {
 
-      $scope.icons = ["graduation-cap", "home", "plane", "car",]
-      // $scope.vm.arrayData = [
-      //   { src: 'image1.png' },
-      //   { src: 'image2.png' },
-      //   { src: 'image3.png' },
-      //   { src: 'image4.png' }
-      // ];
+    $scope.icons = ["graduation-cap", "home", "plane", "car", "bank", "gift", "shopping-bag"]
 
-      $scope.sendData = function() {
-        var data = {
-          name: $scope.goal.name,
-          cost: $scope.goal.cost
-        }
-
-        $http({
-          method: 'POST',
-          url: 'https://thrifty-app.herokuapp.com/newgoal',
-          data: data,
-          headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token},
-          beforeSend: function (xhr) {
-            xhr.setRequestHeader('email', window.localStorage.email)
-            xhr.setRequestHeader('auth_token', window.localStorage.auth_token)
-            // ADD CUTE WAITING UI HERE
-          }
-        })
-        .success( function (data) {
-          console.log("Goal created! " + data)
-          $location.path("/goalplan")
-        })
+    $scope.index = 0
+    $scope.prev = function() {
+      if ($scope.index === 0) {
+        $scope.index = $scope.icons.length - 1;
       }
+      else {
+        $scope.index -= 1;
+      }
+    }
+    $scope.next = function() {
+      $scope.index += 1;
+      if ($scope.index === $scope.icons.length) {
+        $scope.index = 0;
+      }
+    }
+
+    $scope.sendData = function() {
+      var data = {
+        name: $scope.goal.name,
+        cost: $scope.goal.cost
+      }
+
+      $http({
+        method: 'POST',
+        url: 'https://thrifty-app.herokuapp.com/newgoal',
+        data: data,
+        headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token},
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('email', window.localStorage.email)
+          xhr.setRequestHeader('auth_token', window.localStorage.auth_token)
+          // ADD CUTE WAITING UI HERE
+        }
+      })
+      .success( function (data) {
+        console.log("Goal created! " + data)
+        $location.path("/goalplan")
+      })
+    }
   }})
