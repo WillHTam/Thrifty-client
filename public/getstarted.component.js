857bb373faac3e4945c4b7f80c1aa4801dbde3f4
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 10:55:17 +0800

    income validation working. min .

--- public/getstarted.component.js
+++ public/getstarted.component.js
@@ -3,20 +3,33 @@ angular.module('thriftyApp')
     templateUrl: 'getstarted.template.html',
     controller: function($http, $scope, $location) {
       $scope.sendData = function() {
+
         var data = {
           monthly_income: $scope.user.monthly_income
         }
 
-        $http({
-          method: 'PUT',
-          url: 'https://thrifty-app.herokuapp.com/account',
-          data: data,
-          headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token}
-        })
-        .success( function (data) {
-          console.log(data)
-          $location.path("/dashboard")
-        })
-      }
+        $scope.input = $scope.user.monthly_income;
+
+        $scope.$watch('input', function () {
+          if ($scope.input < 99) {
+            console.log($scope.input)
+            console.log("Not enough moneys!")
+           }
+          else if ($scope.input > 99) {
+           console.log($scope.input)
+           $http({
+             method: 'PUT',
+             url: 'https://thrifty-app.herokuapp.com/account',
+             data: data,
+             headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token}
+           })
+           .success( function (data) {
+             console.log("User updated! Proceed to next page.")
+             $location.path("/newgoal")
+           })
+         }
+       });
+
+      } // end sendData function
     }
   })
