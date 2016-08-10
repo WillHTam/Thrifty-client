ac97cd72f0a4b393152cbf74fa8343d0f7b02197
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 09:48:59 +0800

    added registration form validation, added ngMessages to core.js

--- public/core.js
+++ public/core.js
@@ -1,4 +1,4 @@
-var app = angular.module('thriftyApp', ['ngRoute', 'ngMaterial'])
+var app = angular.module('thriftyApp', ['ngRoute', 'ngMaterial', 'ngMessages'])
 
 app.config(function($mdThemingProvider) {
   // Extend the red theme with a different color and make the contrast color black instead of white.
