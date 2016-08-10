1d59b64caf4f42d3a5e465940194603d125b9d7f
William T <william.tam18@gmail.com>
2016-08-10 10:36:57 +0800

    ajax call for dashboard working

--- public/dashboard.component.js
+++ public/dashboard.component.js
@@ -2,9 +2,15 @@ angular.module('thriftyApp')
   .component('dashboard', {
     templateUrl: 'dashboard.template.html',
     controller: function ($http, $scope) {
-      $http.get("https://thrifty-app.herokuapp.com/me")
-        .then(function (response) {
-          $scope.first_name = response.first_name
-        })
+      $http({
+        method: 'GET',
+        url: 'https://thrifty-app.herokuapp.com/user',
+        headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token}
+      })
+      .success( function(response) {
+        console.log(response)
+        $scope.first_name = response.first_name
+        $scope.last_name = response.last_name
+      })
     }
   })
