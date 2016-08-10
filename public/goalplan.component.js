85131c05421add85cc5e108ee943a67708f74b14
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 14:55:02 +0800

    slider added on goalplan

--- public/goalplan.component.js
+++ public/goalplan.component.js
@@ -1,5 +1,15 @@
 angular.module('thriftyApp')
   .component('goalplan', {
     templateUrl: 'goalplan.template.html',
-    controller: function () {}
+    controller: function ($http, $scope) {
+      $http({
+        method: 'GET',
+        url: 'https://thrifty-app.herokuapp.com/user',
+        headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token}
+      })
+      .success( function(response) {
+        console.log(response)
+        $scope.monthly_income = response.monthly_income
+      })
+    }
   })
