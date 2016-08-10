881b84e06c2bebcf0d69f3aeeef482d096f41538
Sprazzeus <sprazzeus@gmail.com>
2016-08-10 17:37:29 +0800

    sliders working

--- public/goalplan.component.js
+++ public/goalplan.component.js
@@ -12,10 +12,11 @@ angular.module('thriftyApp')
     .success( function(response) {
       console.log(response[response.length - 1])
       window.localStorage.goal_id = response[response.length - 1]._id
-      $scope.goal_cost = response[response.length - 1].cost
+      $scope.cost = response[response.length - 1].cost
+      $scope.time_left = $scope.cost / $scope.monthly_budget
     })
 
-    // get user's monthly income
+    // get user's available income
     $http({
       method: 'GET',
       url: 'https://thrifty-app.herokuapp.com/user',
@@ -26,9 +27,19 @@ angular.module('thriftyApp')
       $scope.available_income = response.available_income
     })
 
-
     $scope.monthly_budget = 100
-    $scope.time_left = 1
+
+
+    $scope.monthly_budget_changed = function () {
+      $scope.time_left = Math.ceil($scope.cost / $scope.monthly_budget)
+      console.log("monthly_budget_changed() ran")
+    }
+
+    $scope.time_left_changed = function () {
+      $scope.monthly_budget = Math.ceil($scope.cost / $scope.time_left)
+      console.log("time_left_changed() ran")
+    }
+
     $scope.unit =  function () {
       if ($scope.time_left === 1) {
         return "month"
@@ -37,9 +48,39 @@ angular.module('thriftyApp')
         return "months"
       }
     }
+
+    // if user saves minimum of $100 / month
     $scope.max_time = function () {
-      var max_time = $scope.cost / $scope.monthly_budget
+      var max_time = $scope.cost / 100
       return max_time
     }
-  }
+    $scope.min_time = function () {
+      var min_time = $scope.cost / $scope.available_income
+      return min_time
+    }
+
+    $scope.sendData = function() {
+      var data = {
+        time_left: $scope.time_left,
+        monthly_budget: $scope.monthly_budget,
+      }
+
+      $http({
+        method: 'POST',
+        url: 'https://thrifty-app.herokuapp.com/newgoal',
+        data: data,
+        headers: {'email': window.localStorage.email, 'auth_token': window.localStorage.auth_token},
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('email', window.localStorage.email)
+          xhr.setRequestHeader('auth_token', window.localStorage.auth_token)
+          // ADD CUTE WAITING UI HERE
+        }
+      })
+      .success( function (data) {
+        console.log("Goal created! " + data)
+        $location.path("/dashboard")
+      })
+    }
+
+  } // end controller
 })
